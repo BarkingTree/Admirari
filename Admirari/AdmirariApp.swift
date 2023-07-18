3967adfe6633ad3414847c242a5bcd9d46bf3e5f
Sam House <sam@housey.org>
2023-07-18 11:14:43 +0100

    Initial Commit

---
+++ Admirari/AdmirariApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmirariApp.swift
+//  Admirari
+//
+//  Created by Samuel James House on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdmirariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
